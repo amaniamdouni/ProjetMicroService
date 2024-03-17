bdc9367503a8d5b71e5f8805e0f6ec6cf9b1247b
Eya TRABELSI <eya.trabelsi.1@esprit.tn>
2024-03-17 14:00:24 +0100

    second commit

---
+++ centre/src/test/java/com/esprit/centre/CentreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esprit.centre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
